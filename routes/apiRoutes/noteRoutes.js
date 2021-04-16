17e4e239d4e1fdc5838cdeee5908d45bbb142694
Big Head <spencer.moyer16@yahoo.com>
2021-04-15 22:06:14 -0700

    wrote html routes and began api routes

---
+++ routes/apiRoutes/noteRoutes.js
@@ -0,0 +1,8 @@
+const router = require('express').Router();
+const {notes} = require('../../db/db.json');
+
+router.get('/notes', (req, res) => {
+    res.json(notes);
+});
+
+module.exports = router;
--- server.js
+++ server.js
@@ -7,9 +7,10 @@ const app = express();
 app.use(express.urlencoded({extended: true}));
 app.use(express.json());
 app.use(express.static('public'));
+
 app.use('/api', apiRoutes);
 app.use('/', htmlRoutes);
 
 app.listen(PORT, () => {
     console.log(`Server started on PORT ${PORT}`);
-})
+});
