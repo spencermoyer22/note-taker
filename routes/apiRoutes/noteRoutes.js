0d478dae9f04b42947b678af4493926b59682217
Big Head <spencer.moyer16@yahoo.com>
2021-04-16 11:56:50 -0700

    added function to create new notes

--- routes/apiRoutes/noteRoutes.js
+++ routes/apiRoutes/noteRoutes.js
@@ -1,8 +1,15 @@
 const router = require('express').Router();
 const {notes} = require('../../db/db.json');
+const {addNote} = require('../../lib/notes');
 
 router.get('/notes', (req, res) => {
     res.json(notes);
 });
+
+router.post('/notes', (req, res) => {
+    req.body.id = notes.length.toString();
+    const newNotes = addNote(req.body, notes);
+    res.json(newNotes);
+})
 
 module.exports = router;
