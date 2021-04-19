7fa2f4e098355fc40d69a7b38755ad35dd89a7b1
Big Head <spencer.moyer16@yahoo.com>
2021-04-18 19:30:29 -0700

    added heroku

--- routes/apiRoutes/noteRoutes.js
+++ routes/apiRoutes/noteRoutes.js
@@ -1,6 +1,6 @@
 const router = require('express').Router();
 const {notes} = require('../../db/db.json');
-const {addNote} = require('../../lib/notes');
+const {addNote, deleteNote} = require('../../lib/notes');
 
 router.get('/notes', (req, res) => {
     res.json(notes);
@@ -10,6 +10,11 @@ router.post('/notes', (req, res) => {
     req.body.id = notes.length.toString();
     const newNotes = addNote(req.body, notes);
     res.json(newNotes);
-})
+});
+
+// router.delete('/notes/:id', (req, res) => {
+//     const newArray = deleteNote(req.params.id, notes);
+//     res.json(newArray);
+// });
 
 module.exports = router;
